96c92a2ae7d6f74a8de2cf85d4aa5c3e13c99d34
satyavara90 <133332757+satyavara90@users.noreply.github.com>
2023-10-10 23:35:54 +0530

    Update appservice.tf

--- appservice.tf
+++ appservice.tf
@@ -1,24 +1,24 @@
 
-resource "azurerm_service_plan" "testsatyaphp1234" {
-  name                = "testsatyaphp1234"
+resource "azurerm_service_plan" "testsatyaphp12346" {
+  name                = "testsatyaphp12346"
   resource_group_name = local.resource_group_name
   location            = local.location
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "testsatyaphp1234" {
-  name                = "testsatyaphp1234"
+resource "azurerm_linux_web_app" "testsatyaphp12346" {
+  name                = "testsatyaphp12346"
   resource_group_name = local.resource_group_name
   location            = local.location
-  service_plan_id     = azurerm_service_plan.testsatyaphp1234.id
+  service_plan_id     = azurerm_service_plan.testsatyaphp12346.id
 
   site_config {
 
     always_on                 = true
     application_stack{
 
-        php_version = 8.0
+        php_version = "8.0"
 
     }
     
@@ -26,8 +26,8 @@ resource "azurerm_linux_web_app" "testsatyaphp1234" {
 }
 
 
-resource "azurerm_app_service_source_control" "testsatyaphp1234" {
-  app_id   = azurerm_linux_web_app.testsatyaphp1234.id
+resource "azurerm_app_service_source_control" "testsatyaphp12346" {
+  app_id   = azurerm_linux_web_app.testsatyaphp12346.id
   repo_url = "https://github.com/Azure-Samples/php-docs-hello-world"
   branch   = "master"
 }
