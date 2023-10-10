8a25424a0f6c1b026e7b9a01fd3e07492a1cd6fc
satyavara90 <133332757+satyavara90@users.noreply.github.com>
2023-10-10 22:51:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "subscription_id" {
+  description = "GitHub user name"
+  type        = string
+}
+variable "tenant_id" {
+  description = "GitHub user name"
+  type        = string
+}
+variable "client_id" {
+  description = "GitHub user name"
+  type        = string
+}
