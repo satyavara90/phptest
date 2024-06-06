9cf0b22b4c9201f01bb635b0755aea51bf25eb69
satyavara90 <133332757+satyavara90@users.noreply.github.com>
2024-06-06 22:31:44 +0530

    set24

--- deploy-app.R
+++ deploy-app.R
@@ -1,6 +1,6 @@
 rsconnect::deployApp(
   appDir = "./", 
-  appFileManifest = "manifest.json", 
+  appFileManifest = "./manifest.json", 
   account = Sys.getenv("CONNECT_API_KEY"), 
   server = Sys.getenv("CONNECT_SERVER")
 )
